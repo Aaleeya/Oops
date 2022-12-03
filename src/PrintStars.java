bd7e1f41d39f08dd6e15df5254fb10f7044d76c1
Aaleeya <shaikhaaleeya17@gmail.com>
2022-12-02 21:13:56 -0500

    Print asterisk in pyramid form using for loop program added

--- src/PrintStars.java
+++ src/PrintStars.java
@@ -1,7 +1,17 @@
 public class PrintStars {
     public static void main(String[] args){
-        for(int i=0 ; i<6; i++){ //for row
+
+        //pyramid
+        /*for(int i=0 ; i<5; i++){ //for row
             for(int j=0; j<i;j++) { //for column
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }*/
+
+        //inverted pyramid
+        for(int i=0 ; i<5; i++){ //for row
+            for(int j=5; j>i;j--) { //for column
                 System.out.print("*");
             }
             System.out.println(" ");
