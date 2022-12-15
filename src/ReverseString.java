25176017854e0676fab2eaa1c21b694a2ec9fef4
Aaleeya <shaikhaaleeya17@gmail.com>
2022-12-14 20:40:42 -0500

    factorial program added

---
+++ src/ReverseString.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String text;
+        System.out.println("Enter a String: ");
+        Scanner inputString = new Scanner(System.in);
+        text = inputString.nextLine();
+        StringBuilder input = new StringBuilder();
+        input.append(text);
+        input.reverse();
+        System.out.println("The reversed string is: "+input);
+
+    }
+}
