fdca441c02abcec34e2944c82494ca3e0f768dea
Aaleeya <shaikhaaleeya17@gmail.com>
2022-12-16 07:28:06 -0500

    print all alphabets using loop program added

---
+++ src/Alphabets.java
@@ -0,0 +1,13 @@
+public class Alphabets {
+    public static void main(String[] args) {
+        for(char ch='A'; ch<='Z';ch++){
+            System.out.print(ch+" ");
+        }
+
+        System.out.println();
+
+        for(char c='a'; c<='z';c++){
+            System.out.print(c+" ");
+        }
+    }
+}
